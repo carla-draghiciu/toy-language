8ca1dffd5bb6507c6a48160836a52c175aee7d9c
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-02-03 08:48:45 +0200

    for statement and typecheck

---
+++ src/model/statement/ForStatement.java
@@ -0,0 +1,45 @@
+package model.statement;
+
+import model.adt.MyDictionary;
+import model.exception.MismatchException;
+import model.expression.Expression;
+import model.expression.RelationalExpression;
+import model.expression.VariableExpression;
+import model.state.ProgramState;
+import model.type.IntType;
+import model.type.Type;
+
+public record ForStatement(String varName, Expression exp1, Expression exp2, Expression exp3, Statement statement) implements Statement {
+    // for(v=exp1;v<exp2;v=exp3) stmt
+    @Override
+    public ProgramState execute(ProgramState state) {
+        // int v; v=exp1;(while(v<exp2) stmt;v=exp3) - push the new statement on the stack
+        Statement newSt = new CompoundStatement(
+                new VariableDeclarationStatement(new IntType(), "v"),
+                new CompoundStatement(
+                        new AssignmentStatement("v", exp1),
+                        new WhileStatement(
+                                new RelationalExpression(new VariableExpression("v"), exp2, "<"),
+                                new CompoundStatement(
+                                        statement,
+                                        new AssignmentStatement("v", exp3)
+                                )
+                        )
+                )
+        );
+        state.execStack().push(newSt);
+        return null;
+    }
+
+    // The typecheck method of for statement verifies if exp1, exp2, and exp3 have  the type int.
+    @Override
+    public MyDictionary<String, Type> typecheck(MyDictionary<String,Type> typeEnv) {
+        Type t1 = exp1.typecheck(typeEnv);
+        Type t2 = exp2.typecheck(typeEnv);
+        Type t3 = exp3.typecheck(typeEnv);
+        if (t1 instanceof IntType && t2 instanceof IntType && t3 instanceof IntType) {
+            return typeEnv;
+        }
+        throw new MismatchException("FOR Expressions must be integers");
+    }
+}
